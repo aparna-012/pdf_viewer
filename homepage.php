c33f3a9106f53b3ab9519cb2ac85c61d7954ef31
Aparna Rajaguru <120696832+aparna-012@users.noreply.github.com>
2023-03-31 00:29:02 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	
+	<title>Test 1 - pdf viewer</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<!-----first child---->
+
+<center>
+<h1><em id="greetings">Hey there!! now you can download or check the grade 3 English text book </em></h1>
+<embed src="file/english_grade_3.pdf" height="700" width="1000"></embed>
+
+</center>
+
+<!-----footer-->
+
+<nav class="navbar navbar-dark bg-dark">
+
+
+<div class="container-fluid">
+  	
+    <a class="navbar-brand" href="#" text="center">
+      Aparna @2023-03-31
+    </a>
+  </div>
+  
+  </nav>
+
+ 
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
